7d0968f5842042665393b7f7fd7593b57f24fbcd
Denis Kovalev <aikikode@gmail.com>
2019-11-16 20:46:59 +0200

    Fix merge conflicts

--- app.py
+++ app.py
@@ -2,25 +2,16 @@ import json
 import os
 
 import redis
-<<<<<<< HEAD
-from flask import Flask, request
-from flask_cors import CORS, cross_origin
 import requests
-
-from db import load_mock_data
-=======
 from flask import Flask, request, Response
->>>>>>> 6d909c8b24d0f75fba49236ff0d661663b8daa35
+from flask_cors import CORS, cross_origin
 
-QNA_HOST = 'https://hackjunction.azurewebsites.net/qnamaker'
-QNA_APP_ID = '3e68fd98-d61c-4dd0-aea7-80710112abed'
-QNA_ENDPOINT = f'{QNA_HOST}/knowledgebases/{QNA_APP_ID}/generateAnswer'
+QNA_HOST = "https://hackjunction.azurewebsites.net/qnamaker"
+QNA_APP_ID = "3e68fd98-d61c-4dd0-aea7-80710112abed"
+QNA_ENDPOINT = f"{QNA_HOST}/knowledgebases/{QNA_APP_ID}/generateAnswer"
 
 app = Flask(__name__)
-<<<<<<< HEAD
 CORS(app)
-db = redis.from_url(os.environ.get('REDIS_URL'))
-=======
 db = redis.from_url(os.environ.get("REDIS_URL"))
 
 FIXTURES = os.path.join(os.path.curdir, "fixtures.json")
@@ -32,7 +23,6 @@ def load_mock_data(db):
         for idx, task_data in enumerate(tasks, start=1):
             for prefix in task_data.keys():
                 db.set(f"{prefix}:{idx}", json.dumps(task_data[prefix]))
->>>>>>> 6d909c8b24d0f75fba49236ff0d661663b8daa35
 
 
 @app.route("/api/v1/tasks/<int:task_id>", methods=["GET"])
@@ -56,10 +46,9 @@ def process_answer(task_id):
 @cross_origin()
 def ask_chat():
     data = request.get_json()
-    print(data)
-    payload = {'question': data['message'], 'top': 1}
-    key = os.environ.get('QNA_KEY')
-    headers = {'Authorization': f'EndpointKey {key}'}
+    payload = {"question": data["message"], "top": 1}
+    key = os.environ.get("QNA_KEY")
+    headers = {"Authorization": f"EndpointKey {key}"}
     r = requests.post(QNA_ENDPOINT, json=payload, headers=headers)
     return r.json(), 200
 
