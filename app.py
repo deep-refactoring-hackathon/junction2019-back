c626707dade3ca302cd784d0e67ed31ae89a7f69
Denis Kovalev <aikikode@gmail.com>
2019-11-16 12:05:36 +0200

    Add redis config as env variables (#8)

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ from flask import Flask
 from db import load_mock_data
 
 app = Flask(__name__)
-db = redis.Redis(host="redis", port=6379)
+db = redis.Redis(
+    host=os.environ.get("REDIS_HOST"),
+    port=os.environ.get("REDIS_PORT"),
+    password=os.environ.get("REDIS_PASSWORD"),
+    charset="utf-8",
+    errors="strict",
+)
 
 
 @app.route("/api/v1/tasks/<int:task_id>", methods=["GET"])
@@ -14,8 +20,8 @@ def get_task(task_id):
     return db.get(task_id)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_mock_data(db)
     port = int(os.environ.get("PORT", 5000))
-    host = os.environ.get("HOST", '0.0.0.0')
+    host = os.environ.get("HOST", "0.0.0.0")
     app.run(host=host, port=port)
